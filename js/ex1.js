1706eab63fde8896813b72e40c2320863f49ff15
Brentaldam <brent.marriott@student.csulb.edu>
2023-02-15 21:23:01 -0800

    v1

--- js/ex1.js
+++ js/ex1.js
@@ -1,7 +1,7 @@
-const num1 = String(prompt("Enter first number : "));
-const num2 = String(prompt("Enter second number : "));
-const operand = String(prompt("Enter a Mathematical Operation (+, -, *, /): "));
-ans = "";
+const num1 = Number(prompt("Enter first number : "));
+const num2 = Number(prompt("Enter second number : "));
+var operand = String(prompt("Enter a Mathematical Operation (+, -, *, /): "));
+var ans = "";
 if(operand === "+"){
 ans = num1 + num2;
 }
@@ -14,4 +14,4 @@ else if(operand === "*"){
 else{
     ans = num1 / num2; 
 }
-console.log( num1, operand, num2,"=", ans);
+console.log( `${num1} ${operand} ${num2} = ${ans}`);
